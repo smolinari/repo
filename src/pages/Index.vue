931ce20e443321816cfc65d77a98b45a681fec2e
luckman8 <lucklouis@gmail.com>
2019-08-07 01:52:37 -0700

    First commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div>
+
+    <p>{{message}}</p>
+
+
+    <p>Image as an ASSET</p>
+    <img src="~assets/test.jpg" />
+
+    <q-space/>
+
+    <p>Image as a STATIC</p>
+    <img src="statics/test.jpg"/>
+
+
+  </div>
+
+
+
+</template>
+
+<style>
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+
+
+export default Vue.extend({
+  name: "PageIndex",
+
+
+  data: function () {
+
+    return {
+      message: 'Vue is working'
+    }
+  }
+
+
+}); //Vue.extend
+
+</script>
